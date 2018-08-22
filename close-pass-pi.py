73242bf16628bb0d1a7d421c99f5af6bbbe3c390
Martyn Eggleton <martyn.eggleton@gmail.com>
2018-08-22 13:52:15 +0100

    working with LiDAR

--- close-pass-pi.py
+++ close-pass-pi.py
@@ -1,5 +1,21 @@
 import picamera
 from time import sleep
+import VL53L0X
+from gpiozero import LED
+
+buzzer = LED(21)
+
+# Create a VL53L0X object
+tof = VL53L0X.VL53L0X()
+
+# Start ranging
+tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+
+timing = tof.get_timing()
+if (timing < 20000):
+    timing = 20000
+print ("Timing %d ms" % (timing/1000))
+
 
 i = 1
 minDistance = 0.5
@@ -20,14 +36,21 @@ sensor = DistanceSensor(echo=17, trigger=4)
 try:
     while True:
         camera.wait_recording(0.1)
+        distance = tof.get_distance()
+        if (distance > 0):
+            print ("%d mm, %d cm, %d" % (distance, (distance/10), i))
+        
+        if(distance > 0 and  distance/ 1000 < minDistance):
+            buzzer.blink()
         # TODO : on trigger write buffer to disk.
-        if(sensor.distance > 0 and  sensor.distance < minDistance):
+        #if(sensor.distance > 0 and  sensor.distance < minDistance):
             print("Too Close... Recording");
-            camera.wait_recording(10)
+            camera.wait_recording(5)
             stream.copy_to("/home/pi/Desktop/motion{}.h264".format(i));
             print("Recording Ended");
+            buzzer.off()
             i += 1
-            if i> 2:
+            if i> 10:
                 break
                 # Keep recording for 10 seconds and only then write the
                 # stream to disk
