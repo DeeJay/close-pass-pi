4bbbb95c61eaf4765186b2c929a0f0532077d8cc
Martyn Eggleton <martyn.eggleton@gmail.com>
2018-08-22 12:15:14 +0100

    By Jove it Works

--- close-pass-pi.py
+++ close-pass-pi.py
@@ -1,32 +1,39 @@
-from picamera import PiCamera
+import picamera
 from time import sleep
-camera = PiCamera()
 
-# TODO : start camera
-camera.start_preview(alpha=192)
-sleep(2)
+i = 1
+minDistance = 0.5
 
+#camera.start_preview(alpha=192)
 
 from gpiozero import DistanceSensor
 from time import sleep
 
+# TODO : get ring buffer recording
+camera = picamera.PiCamera()
+stream = picamera.PiCameraCircularIO(camera, seconds=20)
+camera.start_recording(stream, format='h264')
+print("Camera started");
+
 sensor = DistanceSensor(echo=17, trigger=4)
 
-while True:
-    print(sensor.distance)
-    sleep(1)
+try:
+    while True:
+        camera.wait_recording(0.1)
+        # TODO : on trigger write buffer to disk.
+        if(sensor.distance > 0 and  sensor.distance < minDistance):
+            print("Too Close... Recording");
+            camera.wait_recording(10)
+            stream.copy_to("/home/pi/Desktop/motion{}.h264".format(i));
+            print("Recording Ended");
+            i += 1
+            if i> 2:
+                break
+                # Keep recording for 10 seconds and only then write the
+                # stream to disk
+            
+finally:
+    camera.stop_recording()
     
-camera.capture("/home/pi/Desktop/image.jpg")
-camera.stop_preview()
 
-
-# TODO : Sense ultra sound distance.
-
-# TODO : Loop distance sensor
-
-# TODO : trigger something on distance
-
-# TODO : get ring buffer recoirding
-
-# TODO : on trigger write buffer to disk.
 
